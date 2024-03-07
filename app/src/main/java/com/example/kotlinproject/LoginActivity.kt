b9d78023f6e0eab00cc5343283ee41ac589a0341
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-03-07 20:14:50 -0300

    Added first two activities: LoginActivity and HomeActivity. First button to move from one of the to the other.

--- app/src/main/java/com/example/kotlinproject/LoginActivity.kt
+++ app/src/main/java/com/example/kotlinproject/LoginActivity.kt
@@ -1,11 +1,28 @@
 package com.example.kotlinproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            goToHome()
+        }
+
+
     }
+
+    private fun goToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
