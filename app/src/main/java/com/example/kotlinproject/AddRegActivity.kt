88ab448f4d261b8b9da338e1e345e8101e0dd842
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-03-11 22:21:23 -0300

    Added new activity and its fragment to add a new register.

--- app/src/main/java/com/example/kotlinproject/AddRegActivity.kt
+++ app/src/main/java/com/example/kotlinproject/AddRegActivity.kt
@@ -1,14 +1,11 @@
 package com.example.kotlinproject
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 
-class HomeActivity : AppCompatActivity() {
+class AddRegActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_add_reg)
     }
 }
--- app/src/main/java/com/example/kotlinproject/AddRegFragmentAmount.kt
+++ app/src/main/java/com/example/kotlinproject/AddRegFragmentAmount.kt
@@ -8,11 +8,11 @@ import android.view.ViewGroup
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeAddRegFragment.newInstance] factory method to
+ * Use the [AddRegFragmentAmount.newInstance] factory method to
  * create an instance of this fragment.
  * @todo modify
  */
-class HomeAddRegFragment : Fragment() {
+class AddRegFragmentAmount : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +24,6 @@ class HomeAddRegFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_add_reg, container, false)
+        return inflater.inflate(R.layout.fragment_add_reg_amount, container, false)
     }
 }
