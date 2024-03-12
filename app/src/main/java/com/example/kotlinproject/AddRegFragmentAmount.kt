7eb936694a4928b3c490cc325d6d68f00dbe4db7
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-03-12 00:03:05 -0300

    Configured spinner to select currency when adding a new register. Next and cancel drawables added for each corresponding image buttons.

--- app/src/main/java/com/example/kotlinproject/AddRegFragmentAmount.kt
+++ app/src/main/java/com/example/kotlinproject/AddRegFragmentAmount.kt
@@ -1,29 +1,41 @@
 package com.example.kotlinproject
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.Spinner
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddRegFragmentAmount.newInstance] factory method to
- * create an instance of this fragment.
- * @todo modify
- */
 class AddRegFragmentAmount : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_reg_amount, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_add_reg_amount, container, false)
+
+        // Spinner
+        val currencies = listOf("ARS", "USD", "USDT", "BTC")
+        val currencySpinner = rootView.findViewById<Spinner>(R.id.currencySpinner)
+        val spinnerAdapter = ArrayAdapter(requireActivity(), android.R.layout.simple_spinner_item,currencies)
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        currencySpinner.adapter = spinnerAdapter
+
+        // Buttons
+        val nextButton = rootView.findViewById<ImageButton>(R.id.nextButton)
+        val cancelButton = rootView.findViewById<ImageButton>(R.id.cancelButton)
+        nextButton.setOnClickListener {
+            requireActivity()?.finish()
+        }
+        cancelButton.setOnClickListener {
+            requireActivity()?.finish()
+        }
+
+        return rootView
     }
 }
