5015db0316133a3cfcb076d1b59c2a80537dacb6
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-06-10 22:20:47 -0300

    Added Barchart library, jitpack.io repository and features to visualize graphs in ViewGraphsFragment.

--- app/src/main/java/com/example/kotlinproject/fragments/ViewGraphsFragment.kt
+++ app/src/main/java/com/example/kotlinproject/fragments/ViewGraphsFragment.kt
@@ -1,6 +1,6 @@
 package com.example.kotlinproject.fragments
 
-import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -9,36 +9,64 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
-import com.example.kotlinproject.App
 import com.example.kotlinproject.R
-import com.example.kotlinproject.model.AppDataBase
 import com.example.kotlinproject.model.Balance
 import com.example.kotlinproject.model.RecordsProvider
-import com.example.kotlinproject.services.CryptoValuesService
+import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 class ViewGraphsFragment : Fragment() {
     private var balancesByCategory = mutableListOf<Balance>()
     private lateinit var textView1: TextView
     private lateinit var textView2: TextView
     private lateinit var textView3: TextView
+    private var barArrayList = ArrayList<BarEntry>()
 
-    fun updateBalancesTextViews(provider: RecordsProvider):Unit{
+    //fun updateBalancesTextViews(provider: RecordsProvider):Unit{
+    //    if(provider == null)
+    //        return
+    //    val db = provider.getDb()
+    //    if(db.balancesDao().countBalances() > 0){
+    //        balancesByCategory = db.balancesDao().getAll().toMutableList()
+    //        if( balancesByCategory.size > 1) { // At least totals balance and any other
+    //            try {
+    //                textView1.text = balancesByCategory[1].amount.toString()
+    //                textView2.text = balancesByCategory[2].amount.toString()//TODO avoid exception
+    //                textView3.text = balancesByCategory[3].amount.toString()
+    //            }catch(exception: ArrayIndexOutOfBoundsException){
+    //                Log.d("Debugger", "Se accedió a valor fuera del string")
+    //            }
+    //        }
+    //    }
+    //    return
+    //}
+
+    fun updateBalancesBarChart(provider: RecordsProvider):Unit{
         if(provider == null)
             return
         val db = provider.getDb()
         if(db.balancesDao().countBalances() > 0){
             balancesByCategory = db.balancesDao().getAll().toMutableList()
             if( balancesByCategory.size > 1) { // At least totals balance and any other
-                try {
-                    textView1.text = balancesByCategory[1].amount.toString()
-                    textView2.text = balancesByCategory[2].amount.toString()//TODO avoid exception
-                    textView3.text = balancesByCategory[3].amount.toString()
-                }catch(exception: ArrayIndexOutOfBoundsException){
-                    Log.d("Debugger", "Se accedió a valor fuera del string")
+                var i = 2f
+                balancesByCategory.forEach{
+                    barArrayList.add(BarEntry(i,it.qtty.toFloat()))
+                    //barArrayList.add(BarEntry(i,30f))
+                    i++
                 }
             }
         }
         return
+    }
+
+    private fun getData(){
+        barArrayList.add(BarEntry(2f, 10f))
+        barArrayList.add(BarEntry(3f, 10f))
+        barArrayList.add(BarEntry(4f, 10f))
+        barArrayList.add(BarEntry(5f, 10f))
     }
 
     override fun onCreateView(
@@ -49,13 +77,23 @@ class ViewGraphsFragment : Fragment() {
         val rootView = inflater.inflate(R.layout.fragment_view_graphs, container, false)
         val cancelButton = rootView.findViewById<ImageButton>(R.id.cancelButton2)
         val provider = RecordsProvider.getProvider()
-        textView1 = rootView.findViewById<TextView>(R.id.numericValue1)
-        textView2 = rootView.findViewById<TextView>(R.id.numericValue2)
-        textView3 = rootView.findViewById<TextView>(R.id.numericValue3)
         cancelButton.setOnClickListener {
             requireActivity().finish()
         }
-        updateBalancesTextViews(provider)
+
+        val barChart = rootView.findViewById<BarChart>(R.id.barchart)
+        updateBalancesBarChart(provider)
+        val barDataSet = BarDataSet(barArrayList, "Estadisticas")
+        val barData = BarData(barDataSet)
+        barChart.data = barData
+        // Color bar data set
+        barDataSet.colors = (ColorTemplate.COLORFUL_COLORS).toMutableList()
+        // Text color
+        barDataSet.valueTextColor = Color.BLACK
+        // Text size
+        barDataSet.valueTextSize = 16f
+        barChart.description.isEnabled = true
+
 
         return rootView
     }
@@ -64,8 +102,7 @@ class ViewGraphsFragment : Fragment() {
         //activity?.registerReceiver(receiver, intentFilter)
         super.onResume()
         val provider = RecordsProvider.getProvider()
-        updateBalancesTextViews(provider)
+        //updateBalancesTextViews(provider)
 
-        // TOOD dibujar graficos en base a los datos
     }
 }
