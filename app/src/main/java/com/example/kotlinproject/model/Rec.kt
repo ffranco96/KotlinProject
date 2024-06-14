4135a54521cfd9d5ea2809961b29f5377254e954
ELDARS\franco.macen <franco.macen@eldars.com.ar>
2024-06-14 20:09:23 -0300

    Implementation of Comparable by Rec class. Sorted records. Modified size of textbox for date.

--- app/src/main/java/com/example/kotlinproject/model/Rec.kt
+++ app/src/main/java/com/example/kotlinproject/model/Rec.kt
@@ -3,6 +3,7 @@ package com.example.kotlinproject.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
 
 // Used Rec as Record to avoid name conflicts with other classes
 @Entity(tableName = "RECORDS")
@@ -14,4 +15,11 @@ data class Rec (
     @ColumnInfo var date:String = "",
     @ColumnInfo var currency:String = "",
     @PrimaryKey(autoGenerate = true) var id: Int = 0
-)
+): Comparable<Rec>{
+    override fun compareTo(other: Rec): Int {
+        val formatter = SimpleDateFormat("yyyy-MM-dd") // Adjust format if needed
+        val date1 = formatter.parse(date)
+        val date2 = formatter.parse(other.date)
+        return date1.compareTo(date2)
+    }
+}
